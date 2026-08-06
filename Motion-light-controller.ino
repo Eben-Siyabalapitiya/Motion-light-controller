2e84dca48740cf1eef539c492d590e3e1c81ea8b
Eben Siyabalapitiya <105612607+Eben-Siyabalapitiya@users.noreply.github.com>
2026-08-05 23:28:11 -0400

    Enhance motion detection to hold servo position
    Added logic to hold the servo in the up position for a specified duration after detecting motion.

--- Motion-light-controller.ino
+++ Motion-light-controller.ino
@@ -4,8 +4,11 @@ const int PIN_SERVO = 18;
 const int PIN_PIR = 27;
 const int ANGLE_DOWN = 60;
 const int ANGLE_UP = 0;
+const unsigned long HOLD_MS = 5000UL;
 
 Servo servo;
+bool isUp = false;
+unsigned long lastMotionMs = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -15,17 +18,27 @@ void setup() {
   servo.write(ANGLE_DOWN);
   delay(500);
   servo.detach();
+  Serial.println("ready");
 }
 
 void loop() {
   if (digitalRead(PIN_PIR) == HIGH) {
-    Serial.println("motion");
-    servo.attach(PIN_SERVO, 500, 2400);
-    servo.write(ANGLE_UP);
-    delay(5000);
+    lastMotionMs = millis();
+    if (!isUp) {
+      Serial.println("up");
+      servo.attach(PIN_SERVO, 500, 2400);
+      servo.write(ANGLE_UP);
+      isUp = true;
+    }
+  }
+
+  if (isUp && millis() - lastMotionMs > HOLD_MS) {
+    Serial.println("down");
     servo.write(ANGLE_DOWN);
     delay(500);
     servo.detach();
+    isUp = false;
   }
+
   delay(50);
 }
