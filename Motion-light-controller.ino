a2f69af1f245bbfd4a5f0e486d805fd5af8e9d17
Eben Siyabalapitiya <105612607+Eben-Siyabalapitiya@users.noreply.github.com>
2026-08-05 22:27:42 -0400

    Refactor servo angles and update initialization

--- Motion-light-controller.ino
+++ Motion-light-controller.ino
@@ -2,8 +2,10 @@
 
 const int PIN_SERVO = 18;
 const int PIN_PIR = 27;
-const int ANGLE_A = 60;
-const int ANGLE_B = 120;
+
+const int ANGLE_HOME = 60;   // zero / resting position - servo sits here at boot, mount your arm at this angle
+const int ANGLE_PUSH = 120;  // how far it travels from home - raise for more throw, lower for less
+
 const unsigned long COOLDOWN_MS = 5000UL;
 
 Servo servo;
@@ -24,10 +26,10 @@ void setup() {
   pinMode(PIN_PIR, INPUT);
   servo.setPeriodHertz(50);
   servo.attach(PIN_SERVO, 500, 2400);
-  servo.write(ANGLE_A);
-  delay(500);
+  servo.write(ANGLE_HOME);
+  delay(1000);
   servo.detach();
-  Serial.println("ready");
+  Serial.println("ready - servo parked at home");
 }
 
 void loop() {
@@ -35,8 +37,8 @@ void loop() {
     lastTriggerMs = millis();
     Serial.println("motion");
     servo.attach(PIN_SERVO, 500, 2400);
-    sweepTo(ANGLE_B, ANGLE_A);
-    sweepTo(ANGLE_A, ANGLE_B);
+    sweepTo(ANGLE_PUSH, ANGLE_HOME);
+    sweepTo(ANGLE_HOME, ANGLE_PUSH);
     servo.detach();
   }
   delay(50);
