15bfbf37ac767faa3f91a37303b1f6a7756232fc
Eben Siyabalapitiya <105612607+Eben-Siyabalapitiya@users.noreply.github.com>
2026-08-07 23:53:45 -0400

    Add LED control and blink function
    Added LED blinking functionality when the servo moves down.

--- Motion-light-controller.ino
+++ Motion-light-controller.ino
@@ -2,6 +2,7 @@
 
 const int PIN_SERVO = 18;
 const int PIN_PIR = 27;
+const int PIN_LED = 2;
 const int ANGLE_DOWN = 65;
 const int ANGLE_UP = 25;
 const unsigned long HOLD_MS = 5000UL;
@@ -10,9 +11,20 @@ Servo servo;
 bool isUp = false;
 unsigned long lastMotionMs = 0;
 
+void blinkOff() {
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(PIN_LED, HIGH);
+    delay(110);
+    digitalWrite(PIN_LED, LOW);
+    delay(110);
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   pinMode(PIN_PIR, INPUT_PULLDOWN);
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, LOW);
   servo.setPeriodHertz(50);
   servo.attach(PIN_SERVO, 500, 2400);
   servo.write(ANGLE_DOWN);
@@ -26,6 +38,7 @@ void loop() {
     lastMotionMs = millis();
     if (!isUp) {
       Serial.println("up");
+      digitalWrite(PIN_LED, HIGH);
       servo.attach(PIN_SERVO, 500, 2400);
       servo.write(ANGLE_UP);
       isUp = true;
@@ -37,6 +50,7 @@ void loop() {
     servo.write(ANGLE_DOWN);
     delay(500);
     servo.detach();
+    blinkOff();
     isUp = false;
   }
 
