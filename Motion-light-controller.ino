2055a35643b4ee5d90cd4235d9616912a0965a4a
Eben Siyabalapitiya <105612607+Eben-Siyabalapitiya@users.noreply.github.com>
2026-08-08 00:37:25 -0400

    Refactor motion light controller to use blue and red LEDs
    Replace LED with blue and red pins for motion indication. Implement blinking for the red pin when the servo is down.

--- Motion-light-controller.ino
+++ Motion-light-controller.ino
@@ -2,29 +2,26 @@
 
 const int PIN_SERVO = 18;
 const int PIN_PIR = 27;
-const int PIN_LED = 2;
+const int PIN_BLUE = 2;
+const int PIN_RED = 4;
 const int ANGLE_DOWN = 65;
 const int ANGLE_UP = 25;
 const unsigned long HOLD_MS = 5000UL;
+const unsigned long BLINK_MS = 900UL;
 
 Servo servo;
 bool isUp = false;
+bool blinkState = false;
 unsigned long lastMotionMs = 0;
-
-void blinkOff() {
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(PIN_LED, HIGH);
-    delay(110);
-    digitalWrite(PIN_LED, LOW);
-    delay(110);
-  }
-}
+unsigned long lastBlinkMs = 0;
 
 void setup() {
   Serial.begin(115200);
   pinMode(PIN_PIR, INPUT_PULLDOWN);
-  pinMode(PIN_LED, OUTPUT);
-  digitalWrite(PIN_LED, LOW);
+  pinMode(PIN_BLUE, OUTPUT);
+  pinMode(PIN_RED, OUTPUT);
+  digitalWrite(PIN_BLUE, LOW);
+  digitalWrite(PIN_RED, LOW);
   servo.setPeriodHertz(50);
   servo.attach(PIN_SERVO, 500, 2400);
   servo.write(ANGLE_DOWN);
@@ -38,7 +35,8 @@ void loop() {
     lastMotionMs = millis();
     if (!isUp) {
       Serial.println("up");
-      digitalWrite(PIN_LED, HIGH);
+      digitalWrite(PIN_RED, LOW);
+      digitalWrite(PIN_BLUE, HIGH);
       servo.attach(PIN_SERVO, 500, 2400);
       servo.write(ANGLE_UP);
       isUp = true;
@@ -50,8 +48,15 @@ void loop() {
     servo.write(ANGLE_DOWN);
     delay(500);
     servo.detach();
-    blinkOff();
+    digitalWrite(PIN_BLUE, LOW);
     isUp = false;
+    lastBlinkMs = millis();
+  }
+
+  if (!isUp && millis() - lastBlinkMs > BLINK_MS) {
+    lastBlinkMs = millis();
+    blinkState = !blinkState;
+    digitalWrite(PIN_RED, blinkState);
   }
 
   delay(50);
