82f7d957a5702d093b88a9419f87c3d006e87f3f
Eben Siyabalapitiya <105612607+Eben-Siyabalapitiya@users.noreply.github.com>
2026-08-07 23:36:19 -0400

    Change servo angle from 0 to 30 degrees

--- Motion-light-controller.ino
+++ Motion-light-controller.ino
@@ -3,7 +3,7 @@
 const int PIN_SERVO = 18;
 const int PIN_PIR = 27;
 const int ANGLE_DOWN = 50;
-const int ANGLE_UP = 0;
+const int ANGLE_UP = 30;
 const unsigned long HOLD_MS = 5000UL;
 
 Servo servo;
